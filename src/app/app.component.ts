f8051341ac03012f6ddcacc277eddfe2ca5de95c
Yuri Burger <yuriburger@outlook.com>
2018-10-18 01:28:46 +0200

    AuthN added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { OAuthService } from 'angular-oauth2-oidc';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,43 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-azureb2c-demo';
+
+  constructor(private oauthService: OAuthService) {
+    this.configureAuth();
+  }
+
+  private configureAuth(): void {
+    this.oauthService.configure({
+      issuer:
+        'https://yuriburgerb2c.b2clogin.com/11775757-1a8d-431d-8202-531250b9f960/v2.0/',
+      loginUrl:
+        'https://yuriburgerb2c.b2clogin.com/yuriburgerb2c.onmicrosoft.com/oauth2/v2.0/authorize?p=b2c_1_localaccountsignin',
+      logoutUrl:
+        'https://yuriburgerb2c.b2clogin.com/yuriburgerb2c.onmicrosoft.com/oauth2/v2.0/logout?p=b2c_1_localaccountsignin',
+      redirectUri: window.location.origin + '/index.html',
+      clientId: '54653945-60d4-45fa-a4e9-c992c87eb1c5',
+      scope: 'openid',
+      requestAccessToken: false
+    });
+
+    this.oauthService.setStorage(sessionStorage);
+
+    this.oauthService.tryLogin();
+  }
+
+  public login() {
+    this.oauthService.initImplicitFlow();
+  }
+
+  public logoff() {
+    this.oauthService.logOut();
+  }
+
+  public get name() {
+    const claims = this.oauthService.getIdentityClaims();
+    if (!claims) {
+      return null;
+    }
+    return claims['name'];
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { OAuthModule } from 'angular-oauth2-oidc';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +10,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, HttpClientModule, OAuthModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
